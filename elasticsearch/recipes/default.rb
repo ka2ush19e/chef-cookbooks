832ccd1200747c3b86f0f9680392ffb65b007a67
Kazushige Morooka <morokazu@gmail.com>
2014-03-24 02:09:56 +0900

    add chkfongig on

--- elasticsearch/recipes/default.rb
+++ elasticsearch/recipes/default.rb
@@ -7,4 +7,20 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "elasticsearch::yum_repo"
+include_recipe "yum"
+
+yum_repository "elasticsearch-1.0" do
+  description "Elasticsearch repository for 1.0.x packages"
+  baseurl     "http://packages.elasticsearch.org/elasticsearch/1.0/centos/"
+  gpgkey      "http://packages.elasticsearch.org/GPG-KEY-elasticsearch"
+  gpgcheck    true
+  action      :create
+end
+
+package "elasticsearch" do
+  action :install
+end
+
+service "elasticsearch" do
+  action :enable
+end
