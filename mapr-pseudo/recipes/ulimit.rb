d1f9adee1dfc1c1cdf5da1958741e6acf8feb207
Kazushige Morooka <morokazu@gmail.com>
2014-03-25 13:49:00 +0900

    modify ulimit

--- mapr-pseudo/recipes/ulimit.rb
+++ mapr-pseudo/recipes/ulimit.rb
@@ -7,18 +7,15 @@
 # All rights reserved - Do Not Redistribute
 #
 
-cookbook_file "/etc/security/limits.conf" do
-  source "limits.conf"
-  owner  "root"
-  group  "root"
-  mode   0600
-  action :create
-end
-
-cookbook_file "/etc/security/limits.d/90-nproc.conf" do
-  source "90-nproc.conf"
-  owner  "root"
-  group  "root"
-  mode   0600
-  action :create
+ulimit_domain "mapr" do
+  rule do
+    item  :nofile
+    type  :-
+    value 64000
+  end
+  rule do
+    item  :nproc
+    type  :-
+    value 64000
+  end
 end
