77544b6a6c6821f7f569ea27b03c2a06edef7824
Kazushige Morooka <morokazu@gmail.com>
2014-03-26 20:10:22 +0900

    modify plugins recipe

--- elasticsearch/attributes/default.rb
+++ elasticsearch/attributes/default.rb
@@ -1,2 +1,8 @@
 default[:java][:install_flavor] = "openjdk"
 default[:java][:jdk_version]    = "7"
+
+default[:elasticsearch][:plugins] = {
+    "analysis-kuromoji" => "elasticsearch/elasticsearch-analysis-kuromoji/2.0.0",
+    "head"              => "mobz/elasticsearch-head",
+    "HQ"                => "royrusso/elasticsearch-HQ"
+}
--- elasticsearch/recipes/plugins.rb
+++ elasticsearch/recipes/plugins.rb
@@ -6,3 +6,12 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+node[:elasticsearch][:plugins].each do |name, url|
+  execute "install_plugins" do
+    not_if { Dir.exist?("/usr/share/elasticsearch/plugins/#{name}") }
+    user    "root"
+    command "/usr/share/elasticsearch/bin/plugin -i #{url}"
+    action  :run
+  end
+end
