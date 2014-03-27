332d0c61e586e3525b565deaec0f80378a70e41a
Kazushige Morooka <morokazu@gmail.com>
2014-03-27 11:16:16 +0900

    add plugins

--- elasticsearch/attributes/default.rb
+++ elasticsearch/attributes/default.rb
@@ -2,7 +2,9 @@ default[:java][:install_flavor] = "openjdk"
 default[:java][:jdk_version]    = "7"
 
 default[:elasticsearch][:plugins] = {
-    "analysis-kuromoji" => "elasticsearch/elasticsearch-analysis-kuromoji/2.0.0",
-    "head"              => "mobz/elasticsearch-head",
-    "HQ"                => "royrusso/elasticsearch-HQ"
+  "analysis-kuromoji" => "elasticsearch/elasticsearch-analysis-kuromoji/2.0.0",
+  "head"              => "mobz/elasticsearch-head",
+  "HQ"                => "royrusso/elasticsearch-HQ",
+  "inquisitor"        => "polyfractal/elasticsearch-inquisitor",
+  "marvel"            => "elasticsearch/marvel/latest"
 }
--- elasticsearch/recipes/elasticsearch.rb
+++ elasticsearch/recipes/elasticsearch.rb
@@ -7,10 +7,18 @@
 # All rights reserved - Do Not Redistribute
 #
 
+cookbook_file "/etc/security/limits.d/10-elasticsearch.conf" do
+  source   "10-elasticsearch.conf"
+  owner    "root"
+  group    "root"
+  mode     0644
+  action   :create
+end
+
 package "elasticsearch" do
   action :install
 end
 
 service "elasticsearch" do
-  action :enable
+  action [:enable, :start]
 end
