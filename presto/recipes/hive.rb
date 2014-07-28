6139c77ca9b061cc42cb83b45fce760a14ce0b82
Kazushige Morooka <morokazu@gmail.com>
2014-07-28 17:40:36 +0900

    cdh4 -> cdh5

--- presto/recipes/hive.rb
+++ presto/recipes/hive.rb
@@ -9,10 +9,10 @@
 
 include_recipe "yum"
 
-yum_repository "cloudera-cdh4" do
-  description "Cloudera's Distribution for Hadoop, Version 4"
-  baseurl     "http://archive.cloudera.com/cdh4/redhat/6/x86_64/cdh/4/"
-  gpgkey      "http://archive.cloudera.com/cdh4/redhat/6/x86_64/cdh/RPM-GPG-KEY-cloudera"
+yum_repository "cloudera-cdh5" do
+  description "Cloudera's Distribution for Hadoop, Version 5"
+  baseurl     "http://archive.cloudera.com/cdh5/redhat/6/x86_64/cdh/5/"
+  gpgkey      "http://archive.cloudera.com/cdh5/redhat/6/x86_64/cdh/RPM-GPG-KEY-cloudera"
   gpgcheck    true
   action      :create
 end
